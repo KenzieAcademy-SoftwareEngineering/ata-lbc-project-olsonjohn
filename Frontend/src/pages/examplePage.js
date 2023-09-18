0b83eaac5d44ead7f1d3343a0bce1c99a3684e8e
John Olson <john.olson@mykenzie.snhu.edu>
2023-09-18 17:41:31 -0500

    front end base styling

--- Frontend/src/pages/examplePage.js
+++ Frontend/src/pages/examplePage.js
@@ -17,28 +17,13 @@ class ExamplePage extends BaseClass {
      * Once the page has loaded, set up the event handlers and fetch the concert list.
      */
     async mount() {
-        document.getElementById('get-by-id-form').addEventListener('submit', this.onGet);
-        document.getElementById('create-form').addEventListener('submit', this.onCreate);
-        this.client = new ExampleClient();
 
-        this.dataStore.addChangeListener(this.renderExample)
     }
 
     // Render Methods --------------------------------------------------------------------------------------------------
 
     async renderExample() {
-        let resultArea = document.getElementById("result-info");
 
-        const example = this.dataStore.get("example");
-
-        if (example) {
-            resultArea.innerHTML = `
-                <div>ID: ${example.id}</div>
-                <div>Name: ${example.name}</div>
-            `
-        } else {
-            resultArea.innerHTML = "No Item";
-        }
     }
 
     // Event Handlers --------------------------------------------------------------------------------------------------
@@ -47,33 +32,12 @@ class ExamplePage extends BaseClass {
         // Prevent the page from refreshing on form submit
         event.preventDefault();
 
-        let id = document.getElementById("id-field").value;
-        this.dataStore.set("example", null);
-
-        let result = await this.client.getExample(id, this.errorHandler);
-        this.dataStore.set("example", result);
-        if (result) {
-            this.showMessage(`Got ${result.name}!`)
-        } else {
-            this.errorHandler("Error doing GET!  Try again...");
-        }
     }
 
     async onCreate(event) {
         // Prevent the page from refreshing on form submit
         event.preventDefault();
-        this.dataStore.set("example", null);
 
-        let name = document.getElementById("create-name-field").value;
-
-        const createdExample = await this.client.createExample(name, this.errorHandler);
-        this.dataStore.set("example", createdExample);
-
-        if (createdExample) {
-            this.showMessage(`Created ${createdExample.name}!`)
-        } else {
-            this.errorHandler("Error creating!  Try again...");
-        }
     }
 }
 
@@ -82,7 +46,7 @@ class ExamplePage extends BaseClass {
  */
 const main = async () => {
     const examplePage = new ExamplePage();
-    examplePage.mount();
+    await examplePage.mount();
 };
 
 window.addEventListener('DOMContentLoaded', main);
